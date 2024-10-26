5088cdca8bedfca0b43f5e01ecc3993ef400c9b2
Rewriter00x <84532061+Rewriter00x@users.noreply.github.com>
2024-10-26 11:59:32 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -34,35 +34,37 @@ project "GLFW"
 
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
 
-    files
-    {
-        "src/win32_init.c",
-        "src/win32_module.c",
-        "src/win32_joystick.c",
-        "src/win32_monitor.c",
-        "src/win32_time.h",
-        "src/win32_time.c",
-        "src/win32_thread.h",
-        "src/win32_thread.c",
-        "src/win32_window.c",
-        "src/wgl_context.c",
-        "src/egl_context.c",
-        "src/osmesa_context.c"
-    }
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_module.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.h",
+            "src/win32_time.c",
+            "src/win32_thread.h",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+        }
 
-    defines 
-    { 
+        defines 
+        { 
             "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
-    }
+            "_CRT_SECURE_NO_WARNINGS",
+        }
 
     filter { "system:windows", "configurations:Debug" }
         runtime "Debug"
+        symbols "on"
 
     filter { "system:windows", "configurations:Release" }
         runtime "Release"
+        optimize "on"
 
     filter { "system:windows", "configurations:Dist" }
         runtime "Release"
+        optimize "on"
