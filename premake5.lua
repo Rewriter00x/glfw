165aca024f6096968697af3b9628aed6d9ae5024
Rewriter00x <84532061+Rewriter00x@users.noreply.github.com>
2025-02-04 12:26:00 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
-    staticruntime "on"
+    staticruntime "off"
 
     targetdir   ("bin/" .. outputdir .. "/%{prj.name}")
     objdir      ("int/" .. outputdir .. "/%{prj.name}")
